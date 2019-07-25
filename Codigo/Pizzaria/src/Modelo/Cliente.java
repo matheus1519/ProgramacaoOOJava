f97dc5a430f6fd989048af12976e053e2e6bf6bc
Matheus Almeida <50034417+matheus1519@users.noreply.github.com>
2019-07-25 14:57:50 -0300

    CodigoJava + Diagrama de Classe feito na UMLStar

---
+++ Codigo/Pizzaria/src/Modelo/Cliente.java
@@ -0,0 +1,14 @@
+package Modelo;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Cliente extends Pessoa 
+{
+
+	public Cliente(String nome, String cpf, Date data_nascimento) {
+		super(nome, cpf, data_nascimento);
+	}
+		
+}
