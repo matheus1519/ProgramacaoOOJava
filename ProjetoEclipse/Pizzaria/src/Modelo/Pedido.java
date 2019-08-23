126fee59cbb56fe591d5af87e806a41ef055684b
Matheus Almeida <1519academico@gmail.com>
2019-08-22 21:06:28 -0300

    Ajustes de Arquivos e Tela Pedido

---
+++ ProjetoEclipse/Pizzaria/src/Modelo/Pedido.java
@@ -0,0 +1,36 @@
+package modelo;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Pedido {
+
+	private String idPedido;
+	private float valor;
+	private String status;
+
+    public Pedido(float valor, String status) {
+		this.valor = valor;
+		this.status = status;
+	}
+    
+	public float getValor() 
+	{
+		return valor;
+	}
+	public void setValor(float valor)
+	{
+		this.valor = valor;
+	}
+	public String getStatus() 
+	{
+		return status;
+	}
+	public void setStatus(String status)
+	{
+		this.status = status;
+	}
+	
+
+}
